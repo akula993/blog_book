ad3224188f8ad4a4a172327db090febb50b9aa04
akula993 <akula993@gmail.com>
2022-12-12 07:30:48 +0300

    добавил админку менеджер постов в models urls постоил и сделал предстовления в views а также создал базовый цшаблон и шаблоны

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from blog.models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+    context = {
+        'title': 'Главгая страница',
+        'posts': posts,
+    }
+
+    return render(request, 'blog/post/list.html', context)
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month,
+                             publish__day=day)
+    title = Post.title
+    context = {
+        'title': title,
+        'post': post,
+    }
+    return render(request, 'blog/post/detail.html', context)
